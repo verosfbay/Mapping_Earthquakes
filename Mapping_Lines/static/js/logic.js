14fe69a28db9d27016e050504757a14b892fbb16
Veronica Lobkina <you@example.com>
2022-06-19 13:37:47 -0700

    m

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,7 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([36.1733, -120.1794], 5);
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // Coordinates for each point to be used in the line.
 let line = [
---
+++ Mapping_Multiple_Points/static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoib25pY2EiLCJhIjoiY2w0azdrNHM3MGFwODNqbHh0YWU3aGlicCJ9.jlLTBbCc4lWZ6MPQjzY3gQ";
+
